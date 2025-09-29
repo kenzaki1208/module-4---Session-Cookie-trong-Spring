8be2b228cfd93a4de6de1be5df45b62e6545057a
kenzaki1208 <duytrinh25072001@gmail.com>
2025-09-29 10:04:34 +0700

    update project

---
+++ src/main/java/org/example/demo_session_and_cookie_int_spring/practice/p2/model/User.java
@@ -0,0 +1,29 @@
+package org.example.demo_session_and_cookie_int_spring.practice.p2.model;
+
+public class User {
+    private String email;
+    private String password;
+
+    public User() {}
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
